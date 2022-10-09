e553104b4a1b9f8cafe3c7dcc94f3e10afa6a2af
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-10 01:59:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+// import { Navbar } from "./Components/Navbar/Navbar";
+// import { AllRoutes } from "./Routes/AllRoutes";
+// import { Body } from "./Pages/Body/Body";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login"
+
+function App() {
+  return (
+    <div className="App">
+   
+     
+      <Signup/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
