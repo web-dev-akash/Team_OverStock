e38bd460a6d868c97973148a8927ed01ecabb937
web-dev-akash <akumar415129.ak@gmail.com>
2022-10-10 21:34:42 +0530

    Home Page Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import { Navbar } from "./Components/Navbar/Navbar";
-import { AllRoutes } from "./Routes/AllRoutes";
-import { Body } from "./Pages/Body/Body";
+import { Homepage } from "./Pages/Body/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <AllRoutes />
-      <Body />
+      <Homepage />
     </div>
   );
 }
