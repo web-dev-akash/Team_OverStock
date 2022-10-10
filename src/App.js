4d0f084778acc035522c8eb708b6e97328e5f907
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-10 21:36:02 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "./App.css";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login"
 
+
 function App() {
   return (
     <div className="App">
    
-     
+    
       <Signup/>
+
       <Login/>
     </div>
   );
