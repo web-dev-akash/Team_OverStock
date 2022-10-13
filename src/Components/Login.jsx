367e8e32322b3e7374245e7832332bd1b582dbf2
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-13 20:16:05 +0530

    Add files via upload

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,17 +3,17 @@ import { Input } from "@chakra-ui/react"
 import { Box,Button,Link } from "@chakra-ui/react"
 const Login=()=>{
     return <>
-    <Box>
+    <Box  width="40%">
     
-      <Heading fontSize="xl">Sign In</Heading>
-      <Text>Email Address*</Text>
-      <Input  width='322px' placeholder='Email' />
-      <Text>Password*</Text>
-      <Input  width='322px' placeholder='Paasword' />
+      <Heading fontSize="xl"  mt="12%" ml="-30%">Sign In</Heading>
+      <Text mt="12%"  ml="-39%">Email Address*</Text>
+      <Input type="text" autoComplete="off" mt="3%" width='322px' placeholder='Email' />
+      <Text mt="3%" ml="-45%">Password*</Text>
+      <Input type="password" autoComplete="off" mt="3%"  width='322px' placeholder='Paasword' />
       <hr width="23px"></hr>
-      <Button  width='322px' colorScheme='green'>Sign In</Button>
+      <Button mt="7%" width='322px' colorScheme='green'>Sign In</Button>
       <br></br>
-      <Link>Forgot your password?</Link>
+      <Link mt="8%" color="blue">Forgot your password?</Link>
     </Box>
     </>
 }
