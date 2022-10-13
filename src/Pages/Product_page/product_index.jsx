eaef893771c393173a926f1ce32ee97e10118a2d
web-dev-akash <akumar415129.ak@gmail.com>
2022-10-13 21:06:57 +0530

    Commit

--- src/Pages/Product_page/product_index.jsx
+++ src/Pages/Product_page/product_index.jsx
@@ -1,16 +1,18 @@
-import { Grid } from "@chakra-ui/react";
+import {Box, Grid } from "@chakra-ui/react";
 import LeftBars from "./leftSidebar";
 import { Products } from "./product_page";
 import SortDropdown from "./sortDropdown";
 
+
+
 function Product_page() {
-    <>
+    <Box>
         <SortDropdown/>
       <Grid templateColumns="0.3fr 1fr" spacing={20} gap={5}>
         <LeftBars />
         <Products />
       </Grid>
-    </> 
+    </Box> 
 }
 
 export default Product_page;
