86268555352511ad42ae94f096075723375440ae
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-13 20:16:36 +0530

    Add files via upload

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,41 +1,111 @@
 import { Box } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
-import { Input,HStack, Checkbox, CheckboxGroup,Heading} from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-const Signup = () => {
-  return <>
-    
-    <Box>
-    <Heading fontSize="xl">Create Account</Heading>
-      <Text>Email Address*</Text>
-      <Input  width='322px' placeholder='Email' />
-    </Box>
 
-    <HStack spacing={8}>
-      <Text>Create Password*</Text>
-      <Text>Confirm Password*</Text>
-    </HStack>
-    <HStack>
-    <Input  width='170px' placeholder='Paasword' />
-    <Input  width='170px' placeholder='Confirm Password' />
-    </HStack>
-    {/* <Box w='322px' h='60px' bg='gray.100'> */}
-    {/* <Checkbox mr="180px" defaultChecked>
-      
-    </Checkbox>
-    <Text>Sign up today for exclusive offers from Overstock.com delivered right to your inbox**</Text>
-  </Box> */}
-  <Button  width='322px' colorScheme='blue'>Create Account</Button>
- <hr width="23px"></hr>
-  <Button  width='322px' colorScheme='gray'>Continue as Guest</Button>
-  <Text>By creating an account or continuing as a Guest, you</Text>
-    
-    <Text> agree to our Terms & Conditions and Privacy Policy.</Text>
+import {
+  Input,
+  HStack,
+  Checkbox,
+  CheckboxGroup,
+  Heading,
+} from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Divider, Stack } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+function Signup() {
+  const initialvalues = { email: "", password: "" };
+  const [inputValues, setInputValues] = useState(initialvalues);
 
-<Text>Terms & Conditions | Privacy Policy.
+  const handleChange = (inp) => {
+    const { name, value } = inp.target;
+    setInputValues({ ...inputValues, [name]: value });
+    console.log(inputValues);
+  };
+  const handleSignup = (body) => {
+    fetch(`http://localhost:3000/User`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-**You can unsubscribe at any time</Text>
-  </>
-};
+  handleSignup(inputValues);
+  return (
+    <>
+      <Box width="40%" ml="10%">
+        <Box>
+          <Heading fontSize="xl" mt="12%" ml="-30%">
+            Create Account
+          </Heading>
+          <Text mt="12%" ml="-39%">
+            Email Address*
+          </Text>
+          <Input
+            type="text"
+            autoComplete="off"
+            name="email"
+            id="email"
+            width="322px"
+            mt="3%"
+            placeholder="Email"
+            onChange={handleChange}
+            value={inputValues.email}
+          />
+        </Box>
+
+        <Box ml="20%">
+          <HStack mt="3%" spacing={14}>
+            <Text>Create Password*</Text>
+            <Text>Confirm Password*</Text>
+          </HStack>
+          <HStack mt="3%">
+            <Input
+              type="password"
+              autoComplete="off"
+              name="password"
+              id="password"
+              width="160px"
+              placeholder="Paasword"
+              onChange={handleChange}
+              value={inputValues.password}
+            />
+            <Input
+              type="password"
+              autoComplete="off"
+              name="password2"
+              id="password2"
+              width="160px"
+              placeholder="Confirm Password"
+            />
+          </HStack>
+        </Box>
+        <Button onClick={handleSubmit} width="322px" mt="3%" colorScheme="blue">
+          Create Account
+        </Button>
+        <hr width="23px"></hr>
+        <Button width="322px" mt="5%" colorScheme="gray">
+          Continue as Guest
+        </Button>
+
+        <Text mt="4%">
+          By creating an account or continuing as a Guest, you
+        </Text>
+        <Text>agree to our Terms & Conditions and Privacy Policy.</Text>
+
+        <Text mt="3%">Terms & Conditions | Privacy Policy.</Text>
+        <Text>**You can unsubscribe at any time</Text>
+      </Box>
+      <Stack direction="row" h="600px" p={4}>
+        <Divider orientation="vertical" />
+      </Stack>
+    </>
+  );
+}
+
 // bg="tomato"
 export default Signup;
