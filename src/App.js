9f05ce18ee26581d9c0203ea6505af0996c35c2f
PART00007 <parteel_pw2_040@masai.school>
2022-10-14 23:39:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { AllRoutes } from "./Routes/AllRoutes";
 import { Body } from "./Pages/Body/Body";
-import {Cartpage} from "./Pages/CartPage"
+import Cartpage from "./Pages/CartPage"
 
 function App() {
   return (
--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -6,7 +6,7 @@ function Cartpage(){
       <div className="parent">
         <div className="box1">
           <div>
-            <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/8c574877417df3e284db73ec68f559550dcd7df1/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.jpg?impolicy=mediumlow" />
+          <img src="./src/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.webp"/>
           </div>
           <div>
             <p>
@@ -14,10 +14,10 @@ function Cartpage(){
             </p>
   
             <p className="">Cappuccino</p>
-            <p>INR 44,072.35</p>
+            <p><s>INR 44,072.35</s></p>
             <p className="red">Sale INR 37,803.19</p>
             <p>Ends in 1 day 18 hrs 52 min</p>
-            <p className="selected">high selected items</p>
+            <img src="./Capture.png" alt=""/>
             <div>
               <div className="innerBox">
                 <select>
@@ -41,39 +41,65 @@ function Cartpage(){
           </div>
         </div>
         <div className="box2">
-          <div>
-            <h1>Add a Promo Code</h1>
+            <div className="box2_main1">
+                <span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="svg"><path d="M12 2V22" stroke="#2F3337" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M22 12H2" stroke="#2F3337" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                </span>
+              
+                <h3 className="promo">Add a Promo Code</h3>
+              </div>
+          <div className="box2_main2">
+              <div>
+                <h5>(1) item:</h5>
+                <h5>INR 44,072.35</h5>
+              </div>
+              <div>
+                  <p> Savings:</p>
+                <p className="paragraph"> 6,269.16</p>
+              </div>
+              <div>
+                <h5>Subtotal:</h5>
+                <h5> INR 37,803.19</h5>
+              </div>
+              <div>
+                <h3>Your Total:</h3>
+                <h3>37,803.19</h3>
+              </div>
+              <button>Check Out</button>
           </div>
-          <div></div>
+          
         </div>
       </div>
       <h3> Things We Know You'll Love</h3>
+      <button aria-label="View previous products" class="bq"><svg class="cl-icon" fill="#2f3337" height="24" stroke="#2f3337" viewBox="0 0 24 24" width="24"><title>Previous</title><path d="M17 2L7 12l10 10" stroke="inherit" fill="none" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>
       <div className="parent2">
         <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+        <img src="../src/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.webp"/>
           <button>Add to Cart</button>
           <h3> Sale $373.49</h3>
           <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
         </div>
         <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+        <img src="../src/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.webp"/>
           <button>Add to Cart</button>
           <h3> Sale $373.49</h3>
           <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
         </div>
         <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+        <img src="../src/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.webp"/>
           <button>Add to Cart</button>
           <h3> Sale $373.49</h3>
           <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
         </div>
         <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+        <img src="../src/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.webp"/>
           <button>Add to Cart</button>
           <h3> Sale $373.49</h3>
           <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
         </div>
       </div>
+  
+      <button aria-label="View next products" class="bs" disabled=""><svg class="cl-icon" fill="#2f3337" height="24" stroke="#2f3337" viewBox="0 0 24 24" width="24"><title>Next</title><path d="M7 2l10 10L7 22" stroke="inherit" fill="none" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>
       </div>
     )
   }
