60bde04602a553a61066dd85ca6c4e11270130dd
web-dev-akash <akumar415129.ak@gmail.com>
2022-10-17 00:10:11 +0530

    on going commit

--- src/Pages/Cart/CartPage.jsx
+++ src/Pages/Cart/CartPage.jsx
@@ -1,57 +1,67 @@
-import "./checkout.css"
-function Cartpage(){
-    return (
-      <div className="marginTop"> 
-       <h1 className="heading">Shopping Cart</h1>
-  
-      <div className="parent">
-        <div className="box1">
-          <div>
-            <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/8c574877417df3e284db73ec68f559550dcd7df1/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.jpg?impolicy=mediumlow" />
-          </div>
-          <div>
-            <p>
-              Furniture of America Diz Modern 47-inch Wine Bar Dining Server
-            </p>
-  
-            <p className="">Cappuccino</p>
-            <p>INR 44,072.35</p>
-            <p className="red">Sale INR 37,803.19</p>
-            <p>Ends in 1 day 18 hrs 52 min</p>
-            <p className="selected">high selected items</p>
+import { Box, Center, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { AiOutlinePlus } from "react-icons/ai";
+import "./checkout.css";
+function Cartpage() {
+  return (
+    <Container maxW={"7xl"}>
+      <div className="marginTop">
+        <h1 className="heading">Shopping Cart</h1>
+
+        <div className="parent">
+          <div className="box1">
             <div>
-              <div className="innerBox">
-                <select>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                </select>
+              <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/8c574877417df3e284db73ec68f559550dcd7df1/Furniture-of-America-Diz-Modern-47-inch-Wine-Bar-Dining-Server.jpg?impolicy=mediumlow" />
+            </div>
+            <div>
+              <div className="box1_Div2">
+                <p>
+                  Furniture of America Diz Modern 47-inch Wine Bar Dining Server
+                </p>
+
+                <p className="">Cappuccino</p>
+                <p>INR 44,072.35</p>
+                <p className="red">Sale INR 37,803.19</p>
+                <p>Ends in 1 day 18 hrs 52 min</p>
+                <p className="selected"> High Satisfaction Item</p>
               </div>
-              <div className="innerBox">
-                <u>
-                  {" "}
-                  <button>Remove</button>{" "}
-                </u>
-                <u>
-                  {" "}
-                  <button>Save for later</button>
-                </u>
+              <div>
+                <div className="innerBox">
+                  <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                  </select>
+                </div>
+                <div className="innerBox2">
+                  <u>
+                    {" "}
+                    <button>Remove</button>{" "}
+                  </u>
+                  <u>
+                    {" "}
+                    <button>Save for later</button>
+                  </u>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="box2">
-          <div>
-            <h1>Add a Promo Code</h1>
-          </div>
-          <div className="box2_main2">
+          <div className="box2">
+            <Box border={"1px solid #cecece"}>
+              <Flex align={"center"} justifyContent={"center"} gap={5}>
+                <Text fontSize={"25px"} pos={"relative"} top={"-5px"}>
+                  <AiOutlinePlus />
+                </Text>{" "}
+                <h1>Add a Promo Code</h1>
+              </Flex>
+            </Box>
+            <div className="box2_main2">
               <div>
                 <h5>(1) item:</h5>
                 <h5>INR 44,072.35</h5>
               </div>
               <div>
-                  <p> Savings:</p>
+                <p> Savings:</p>
                 <p className="paragraph"> 6,269.16</p>
               </div>
               <div>
@@ -63,37 +73,38 @@ function Cartpage(){
                 <h3>37,803.19</h3>
               </div>
               <button>Check Out</button>
+            </div>
+          </div>
+        </div>
+        <h3> Things We Know You'll Love</h3>
+        <div className="parent2">
+          <div>
+            <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+            <button>Add to Cart</button>
+            <h3> Sale $373.49</h3>
+            <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
+          </div>
+          <div>
+            <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+            <button>Add to Cart</button>
+            <h3> Sale $373.49</h3>
+            <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
+          </div>
+          <div>
+            <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+            <button>Add to Cart</button>
+            <h3> Sale $373.49</h3>
+            <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
+          </div>
+          <div>
+            <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
+            <button>Add to Cart</button>
+            <h3> Sale $373.49</h3>
+            <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
           </div>
         </div>
       </div>
-      <h3> Things We Know You'll Love</h3>
-      <div className="parent2">
-        <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
-          <button>Add to Cart</button>
-          <h3> Sale $373.49</h3>
-          <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
-        </div>
-        <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
-          <button>Add to Cart</button>
-          <h3> Sale $373.49</h3>
-          <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
-        </div>
-        <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
-          <button>Add to Cart</button>
-          <h3> Sale $373.49</h3>
-          <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
-        </div>
-        <div>
-          <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/6f028d80157c02d1a519d36a0045fa8ef914efb0/Furniture-of-America-Transitional-Espresso-Dining-Buffet.jpg?impolicy=mediumlow" />
-          <button>Add to Cart</button>
-          <h3> Sale $373.49</h3>
-          <p>Furniture of America Rustic Espresso Dining Buffet with Wine</p>
-        </div>
-      </div>
-      </div>
-    )
-  }
-  export default Cartpage
+    </Container>
+  );
+}
+export default Cartpage;
