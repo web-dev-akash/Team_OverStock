52c3f38589383643583d3e63bba581e72ea60de9
PART00007 <parteel_pw2_040@masai.school>
2022-10-16 21:15:20 +0530

    new page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { AllRoutes } from "./Routes/AllRoutes";
 import { Body } from "./Pages/Body/Body";
-import Cartpage from "./Pages/CartPage"
+import Cartpage from "./Pages/Body/CartPage"
+import CheckoutPage from "./Pages/Body/CheckOut";
 
 function App() {
   return (
     <div className="App">
-      
-      <Cartpage/>
+    <Cartpage/>
+
     </div>
   );
 }
